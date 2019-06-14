d21dc93b291b70ecb52cfc1ff6a05757766e2f54
Vaeliwen <zaman@imbriaco.com>
2019-06-14 12:02:22 -0400

    Added axios functions for rendering and adding of smurfs.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,20 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+
+    Axios
+      .post(`http://localhost:5000/smurfs`, newSmurf)
+        .then(response => {
+          this.setState({
+            smurfs: response.data
+          })
+        })
 
     this.setState({
       name: '',
